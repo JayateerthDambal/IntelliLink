824d1c2bfcadc7dcc057885a9d80bcbf1bc615c2
Jayateerth Dambal <jayateerth.dambal@gmail.com>
2021-06-08 13:19:48 +0530

    Modified Settings

--- IntelliLink/settings.py
+++ IntelliLink/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fsyar@7jvcdo5*l(vfq&zmkc#16uw8ui_=0y@f@5tqgifkr^(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Truecrea
+DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.135']
+ALLOWED_HOSTS = ['192.168.43.135', '127.0.0.1']
 
 
 # Application definition
